5007aadada67424e7a67af67a6afd2a5defa9f36
sajihassa <gging2105@gmail.com>
2019-09-22 11:14:29 +0700

    Add TamNai Test

--- maha-lap.js
+++ maha-lap.js
@@ -1,6 +1,28 @@
 class MahaLap {
   static tamnaiWithDay(day) {
-    return 2;
+    switch (true) {
+      case day === "จันทร์":
+        return 2;
+      case day === "อังคาร":
+        return 3;
+      case day === "พุธ":
+        return 4;
+      case day === "พฤหัสบดี":
+        return 5;
+      case day === "ศุกร์":
+        return 6;
+      case day === "เสาร์":
+        return 7;
+      case day === "สงกรานต์":
+        return 13;
+      case day === "ฮาโลวีน":
+        return 31;
+      case day === "คริสมาสต์":
+        return 25;
+      default:
+        return 1;
+    }
   }
 }
+
 module.exports = MahaLap;
--- maha-lap.test.js
+++ maha-lap.test.js
@@ -4,4 +4,48 @@ describe("Test Maha-Lap", () => {
     const result = MahaLap.tamnaiWithDay("จันทร์");
     expect(result).toEqual(2);
   });
+  test("Test Tamnai wiht Tuesday", () => {
+    const result = MahaLap.tamnaiWithDay("อังคาร");
+    expect(result).toEqual(3);
+  });
+
+  test("Test Tamnai wiht Sunday", () => {
+    const result = MahaLap.tamnaiWithDay("อาทิตย์");
+    expect(result).toEqual(1);
+  });
+
+  test("Test Tamnai wiht Wednesday", () => {
+    const result = MahaLap.tamnaiWithDay("พุธ");
+    expect(result).toEqual(4);
+  });
+
+  test("Test Tamnai wiht Thursday", () => {
+    const result = MahaLap.tamnaiWithDay("พฤหัสบดี");
+    expect(result).toEqual(5);
+  });
+
+  test("Test Tamnai wiht Friday", () => {
+    const result = MahaLap.tamnaiWithDay("ศุกร์");
+    expect(result).toEqual(6);
+  });
+
+  test("Test Tamnai wiht Saturday", () => {
+    const result = MahaLap.tamnaiWithDay("เสาร์");
+    expect(result).toEqual(7);
+  });
+
+  test("Test Tamnai wiht Songkran", () => {
+    const result = MahaLap.tamnaiWithDay("สงกรานต์");
+    expect(result).toEqual(13);
+  });
+
+  test("Test Tamnai wiht halloween", () => {
+    const result = MahaLap.tamnaiWithDay("ฮาโลวีน");
+    expect(result).toEqual(31);
+  });
+
+  test("Test Tamnai wiht Christmas", () => {
+    const result = MahaLap.tamnaiWithDay("คริสมาสต์");
+    expect(result).toEqual(25);
+  });
 });
